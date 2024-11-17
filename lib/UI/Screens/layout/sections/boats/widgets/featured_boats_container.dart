a2d08b4bb34c0fba6087ae5c41b4cac15ab00628
Adeeb Abubacker <adeebabu655@gmail.com>
2024-11-17 11:12:05 +0530

    Api class created

--- lib/UI/Screens/layout/sections/boats/widgets/featured_boats_container.dart
+++ lib/UI/Screens/layout/sections/boats/widgets/featured_boats_container.dart
@@ -56,234 +56,6 @@ List<ExploreDestinationmodel> exploreDestination = [
   ),
 ];
 
-class FeaturedBoatsSection extends StatefulWidget {
-  const FeaturedBoatsSection({
-    super.key,
-  });
-
-  @override
-  State<FeaturedBoatsSection> createState() => _FeaturedBoatsSectionState();
-}
-
-class _FeaturedBoatsSectionState extends State<FeaturedBoatsSection> {
-  // List to hold the scale for each image container
-  List<double> _scales = [];
-
-  @override
-  void initState() {
-    super.initState();
-
-    _scales = List.generate(exploreDestination.length, (index) => 1.0);
-  }
-
-  void onTapDown(int index, TapDownDetails details) {
-    setState(() {
-      _scales[index] = 0.94;
-    });
-  }
-
-  void onTapUp(int index, TapUpDetails details) {
-    setState(() {
-      _scales[index] = 1.0;
-    });
-  }
-
-  void onTapCancel(int index) {
-    setState(() {
-      _scales[index] = 1.0;
-    });
-  }
-
-// Handle the tap without parameters for `onTap`
-  void handleTap(int index) {
-    onTapDown(index, TapDownDetails()); // Handle tap down with no details
-    // You can reset the scale after a delay if needed
-    Future.delayed(const Duration(milliseconds: 150), () {
-      setState(() {
-        _scales[index] = 1.0; // Reset the scale after the animation duration
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: ListView.builder(
-        physics: BouncingScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.only(
-              left: index == 0
-                  ? 30
-                  : 10, // Apply 30 for the first item, 10 for others
-            ),
-            child: Stack(
-              children: [
-                GestureDetector(
-                  onTapDown: (details) => onTapDown(index, details),
-                  onTapUp: (details) => onTapUp(index, details),
-                  onTapCancel: () => onTapCancel(index),
-                  onTap: () => handleTap(index),
-                  child: AnimatedScale(
-                    scale: _scales[index],
-                    duration: const Duration(milliseconds: 150),
-                    curve: Curves.easeInOut,
-                    child: Container(
-                      width: 240,
-                      height: 300,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(13),
-                        border: Border.all(
-                          color: Color(0xFFE2E2E2),
-                          width: 1.5,
-                        ),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 100,
-                            child: Stack(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(13),
-                                    topRight: Radius.circular(13),
-                                  ),
-                                  child: Image.asset(
-                                    "assets/image/onboarding_img/onboarding_one.png",
-                                    width: double.infinity,
-                                    height: 100,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                Positioned(
-                                  top: 60,
-                                  right: 8,
-                                  child: Container(
-                                    width: 68,
-                                    height: 30,
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius:
-                                            BorderRadius.circular(24)),
-                                    child: Row(
-                                      children: [
-                                        SizedBox(width: 10),
-                                        Icon(
-                                          Icons.star,
-                                          color: Colors.amber,
-                                          size: 24,
-                                        ),
-                                        Text("4.3"),
-                                        SizedBox(width: 10),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            color: const Color.fromARGB(0, 255, 214, 64),
-                            height: 195,
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 12),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(height: 10),
-                                  Row(
-                                    children: [
-                                      PillWidget(
-                                        image: 'assets/icons/wifi.svg',
-                                        text: 'Wifi',
-                                      ),
-                                      SizedBox(
-                                        width: 5,
-                                      ),
-                                      PillWidget(
-                                        image: 'assets/icons/heater.svg',
-                                        text: 'Heater',
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    "Kerala’s Heritage Haven – Traditional Kerala Décor",
-                                    style: TextStyles.ubuntu16black15w500,
-                                  ),
-                                  Row(
-                                    children: [
-                                      Text(
-                                        "₹5000",
-                                        style: TextStyles.ubuntu18bluew700,
-                                      ),
-                                      Spacer(),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 8,
-                  right: 8,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(25)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Icon(
-                        Icons.favorite,
-                        color: Color(0XFF4FC2C5),
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 35,
-                  right: 8,
-                  child: SizedBox(
-                    height: 45,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0XFF1F8386),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                      ),
-                      child: Text(
-                        "Book Now",
-                        style: TextStyles.ubuntu12whiteFFw400,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
 class PillWidget extends StatelessWidget {
   final String image;
   final String text;
@@ -326,6 +98,238 @@ class PillWidget extends StatelessWidget {
           ),
           SizedBox(width: 10),
         ],
+      ),
+    );
+  }
+}
+
+class FeaturedBoatsSection extends StatefulWidget {
+  const FeaturedBoatsSection({
+    super.key,
+  });
+
+  @override
+  State<FeaturedBoatsSection> createState() => _FeaturedBoatsSectionState();
+}
+
+class _FeaturedBoatsSectionState extends State<FeaturedBoatsSection> {
+  List<double> _scales = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _scales =
+        List.generate(10, (index) => 1.0); // Adjust for the number of items
+  }
+
+  void onTapDown(int index, TapDownDetails details) {
+    setState(() {
+      _scales[index] = 0.94;
+    });
+  }
+
+  void onTapUp(int index, TapUpDetails details) {
+    setState(() {
+      _scales[index] = 1.0;
+    });
+  }
+
+  void onTapCancel(int index) {
+    setState(() {
+      _scales[index] = 1.0;
+    });
+  }
+
+  // Handle the tap without parameters for `onTap`
+  void handleTap(int index) {
+    onTapDown(index, TapDownDetails());
+    Future.delayed(const Duration(milliseconds: 150), () {
+      setState(() {
+        _scales[index] = 1.0; // Reset the scale after the animation duration
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(
+              left: index == 0 ? 30 : 10,
+            ),
+            child: GestureDetector(
+              onTapDown: (details) => onTapDown(index, details),
+              onTapUp: (details) => onTapUp(index, details),
+              onTapCancel: () => onTapCancel(index),
+              onTap: () => handleTap(index),
+              child: AnimatedScale(
+                scale: _scales[index],
+                duration: const Duration(milliseconds: 150),
+                curve: Curves.easeInOut,
+                child: Container(
+                  width: 240,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(13),
+                    border: Border.all(
+                      color: const Color(0xFFE2E2E2),
+                      width: 1.5,
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 100,
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(13),
+                                    topRight: Radius.circular(13),
+                                  ),
+                                  child: Image.asset(
+                                    "assets/image/onboarding_img/onboarding_one.png",
+                                    width: double.infinity,
+                                    height: 100,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 60,
+                                  right: 8,
+                                  child: Container(
+                                    width: 68,
+                                    height: 30,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(24),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        const SizedBox(width: 10),
+                                        const Icon(
+                                          Icons.star,
+                                          color: Colors.amber,
+                                          size: 24,
+                                        ),
+                                        const Text("4.3"),
+                                        const SizedBox(width: 10),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            color: const Color.fromARGB(0, 255, 214, 64),
+                            height: 195,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 12),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    children: [
+                                      PillWidget(
+                                        image: 'assets/icons/wifi.svg',
+                                        text: 'Wifi',
+                                      ),
+                                      const SizedBox(width: 5),
+                                      PillWidget(
+                                        image: 'assets/icons/heater.svg',
+                                        text: 'Heater',
+                                      ),
+                                    ],
+                                  ),
+                                  Text(
+                                    "Kerala’s Heritage Haven – Traditional Kerala Décor",
+                                    style: TextStyles.ubuntu16black15w500,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "₹5000",
+                                        style: TextStyles.ubuntu18bluew700,
+                                      ),
+                                      const Spacer(),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      // Positioned items
+                      Positioned(
+                        top: 8,
+                        right: 8,
+                        child: InkWell(
+                          onTap: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.all(5.0),
+                                child: Icon(
+                                  Icons.favorite,
+                                  color: Color(0XFF4FC2C5),
+                                  size: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 35,
+                        right: 8,
+                        child: SizedBox(
+                          height: 45,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0XFF1F8386),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 12,
+                              ),
+                            ),
+                            child: Text(
+                              "Book Now",
+                              style: TextStyles.ubuntu12whiteFFw400,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
